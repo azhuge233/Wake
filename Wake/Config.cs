9232539291b8c8d3ce2b287bb5fc7ef5cc7090d1
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2024-11-14 20:38:35 +0800

    Add dynamic broadcast ip calculating.

--- Wake/Config.cs
+++ Wake/Config.cs
@@ -1,13 +1,6 @@
 ﻿using System.Text.Json.Serialization;
 
 namespace Wake {
-	public class Config {
-		[JsonPropertyName("BroadcastIP")]
-		public string BroadcastIP { get; set; }
-		
-		[JsonPropertyName("PCList")]
-		public List<WakePC> WakePCs { get; set; }
-	}
 	public class WakePC {
 		[JsonPropertyName("NickName")]
 		public string Nickname { get; set; }
--- Wake/Program.cs
+++ Wake/Program.cs
@@ -2,18 +2,19 @@
 using System.Net.Sockets;
 using System.Net;
 using System.Text.Json;
+using System.Net.NetworkInformation;
 
 namespace Wake {
 	internal class Program {
 		static readonly string ConfigPath = $"{AppDomain.CurrentDomain.BaseDirectory}config.json";
 
+		private static readonly string[] NicBlacklist = { "vethernet", "vmware" };
+
 		static void Main(string[] args) {
 			try {
 				var command = args.FirstOrDefault(string.Empty).ToLower();
 
-				var config = Load();
-				var broadcastIP = config.BroadcastIP;
-				var pcs = config.WakePCs;
+				var pcs = Load();
 				var nicknames = pcs.Select(pc => pc.Nickname.ToLower()).ToHashSet();
 
 				if (args.Length == 0) {
@@ -34,7 +35,7 @@ namespace Wake {
 						break;
 					default:
 						var pc = pcs.First(pc => pc.Nickname.ToLower().Equals(command));
-						Wake(broadcastIP, pc);
+						Wake(pc);
 						break;
 				}
 			} catch (Exception) {
@@ -42,9 +43,9 @@ namespace Wake {
 			}
 		}
 
-		static Config Load() {
+		static List<WakePC> Load() {
 			try {
-				var pcs = JsonSerializer.Deserialize<Config>(File.ReadAllText(ConfigPath));
+				var pcs = JsonSerializer.Deserialize<List<WakePC>>(File.ReadAllText(ConfigPath));
 				return pcs;
 			} catch (Exception ex) {
 				Output.Error(ex.ToString());
@@ -57,22 +58,17 @@ namespace Wake {
 				$"MAC: {pc.MAC}{Output.NewLine}"));
 		}
 
-		static void Wake(string broadcastIP, WakePC pc) {
+		static void Wake(WakePC pc) {
 			try {
-				var ipAddress = broadcastIP;
+				var broadcastIP = GetBroadcastIP();
 				var macAddress = pc.MAC;
 
 				var udpClient = new UdpClient() {
 					EnableBroadcast = true
 				};
-				udpClient.Connect(IPAddress.Parse(ipAddress), new Random().Next(20000, 60000));
+				udpClient.Connect(broadcastIP, 7);
 
-				var magicPacket = Enumerable.Repeat(Convert.ToByte(255), 17 * 6).ToArray();
-				byte[] macAddrBytes = macAddress.Split([':', '-']).Select(str => byte.Parse(str, NumberStyles.HexNumber)).ToArray();
-
-				for (int i = 1; i <= 16; i++)
-					for (int j = 0; j < 6; j++)
-						magicPacket[i * 6 + j] = macAddrBytes[j];
+				var magicPacket = BuildPacket(pc.MAC);
 
 				udpClient.Send(magicPacket, magicPacket.Length);
 				udpClient.Close();
@@ -80,6 +76,40 @@ namespace Wake {
 				Output.Error(ex.ToString());
 				throw;
 			}
+		}
+
+		static IPAddress GetBroadcastIP() {
+			var nic = NetworkInterface.GetAllNetworkInterfaces().First(nic => 
+				nic.NetworkInterfaceType != NetworkInterfaceType.Loopback &&
+				nic.NetworkInterfaceType != NetworkInterfaceType.Tunnel &&
+				!NicBlacklist.Any(name => nic.Name.StartsWith(name)) &&
+				nic.SupportsMulticast &&
+				nic.OperationalStatus == OperationalStatus.Up &&
+				nic.GetIPProperties().GetIPv4Properties != null &&
+				nic.GetIPProperties().MulticastAddresses.Count > 0) ?? throw new Exception("No nics found!");
+
+			var ipv4Addr = nic.GetIPProperties().UnicastAddresses.First(addr => addr.Address.AddressFamily == AddressFamily.InterNetwork);
+
+			var ip = ipv4Addr.Address.GetAddressBytes();
+			var mask = ipv4Addr.IPv4Mask.GetAddressBytes();
+
+			byte[] broadcastIPBytes = new byte[4];
+
+			for (int i = 0; i < 4; i++)
+				broadcastIPBytes[i] = (byte)(ip[i] | ~mask[i]);
+
+			return new IPAddress(broadcastIPBytes);
+		}
+
+		static byte[] BuildPacket(string macAddress) {
+			var magicPacket = Enumerable.Repeat(Convert.ToByte(255), 17 * 6).ToArray();
+			byte[] macAddrBytes = macAddress.Split([':', '-']).Select(str => byte.Parse(str, NumberStyles.HexNumber)).ToArray();
+
+			for (int i = 1; i <= 16; i++)
+				for (int j = 0; j < 6; j++)
+					magicPacket[i * 6 + j] = macAddrBytes[j];
+
+			return magicPacket;
 		}
 	}
 }
